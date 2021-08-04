8cb03a093ecafe85372b47e57234d6bfc4017a43
Ron_Tayler <max.urs@yandex.ru>
2021-08-05 01:02:12 +0300

    Настройки для DEBUG

--- core_config.php
+++ core_config.php
@@ -7,3 +7,20 @@ define('DIR_LIB',DIR_SITE.'/lib');
 define('DIR_MODEL',DIR_SITE.'/model');
 define('DIR_VIEW',DIR_SITE.'/view');
 define('DIR_LOGS',DIR_SITE.'/logs');
+
+define('DEBUG_DUMP',1);
+define('DEBUG_GET',1<<1);
+define('DEBUG_POST',1<<2);
+$DEBUG_CODES = [
+    'dump'=>DEBUG_DUMP,
+    'get'=>DEBUG_GET,
+    'post'=>DEBUG_POST
+];
+
+$DEBUG_CODES['all'] = 0;
+foreach($DEBUG_CODES as $debug_code){
+    $DEBUG_CODES['all'] |= $debug_code;
+}
+
+define('DEBUG_CODES',$DEBUG_CODES);
+unset($DEBUG_CODES,$DEBUG_CODE_ALL,$debug_code);
