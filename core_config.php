b48cdac1b47dd3b68e1d5a7b9cd3444bd7bf46a6
Ron_Tayler <max.urs@yandex.ru>
2021-08-05 00:45:10 +0300

    Init

---
+++ core_config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DIR_SITE',__DIR__);
+define('DIR_SYSTEM',DIR_SITE.'/system');
+define('DIR_CONTROLLER',DIR_SITE.'/controller');
+define('DIR_LIB',DIR_SITE.'/lib');
+define('DIR_MODEL',DIR_SITE.'/model');
+define('DIR_VIEW',DIR_SITE.'/view');
+define('DIR_LOGS',DIR_SITE.'/logs');
